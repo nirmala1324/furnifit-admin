62f4c912c3461b2ea5c0388651d1d51ec109d7fb
unknown <nirmalapuspa1324@gmail.com>
2024-04-16 08:18:57 +0700

    Add chart

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -2,9 +2,27 @@ import { useEffect, useState } from "react";
 import "../styles/dashboard_page.scss";
 import axios from "axios";
 
+// SWIPER
+import React, { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// CHART
+import { PieChart } from "@mui/x-charts/PieChart";
+const data = [
+  { id: 0, value: 10, label: "series A" },
+  { id: 1, value: 15, label: "series B" },
+  { id: 2, value: 20, label: "series C" },
+];
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
 // Importing icons
-import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
-import ListAltRoundedIcon from '@mui/icons-material/ListAltRounded';
+import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded";
+import ListAltRoundedIcon from "@mui/icons-material/ListAltRounded";
 
 const DashboardPage = () => {
   // VARIABLES
@@ -44,11 +62,15 @@ const DashboardPage = () => {
           <div className="logo"></div>
           <div className="menu-container">
             <div className="dash-menu selected-menu">
-              <div className="icon"><DashboardRoundedIcon sx={{ fontSize:30 }}/></div>
+              <div className="icon">
+                <DashboardRoundedIcon sx={{ fontSize: 30 }} />
+              </div>
               <div className="menu">Dashboard</div>
             </div>
             <div className="dash-menu">
-              <div className="icon"><ListAltRoundedIcon sx={{ fontSize:32 }}/></div>
+              <div className="icon">
+                <ListAltRoundedIcon sx={{ fontSize: 32 }} />
+              </div>
               <div className="menu">Furniture Data</div>
             </div>
           </div>
@@ -69,10 +91,47 @@ const DashboardPage = () => {
               <div className="data-table"></div>
             </div>
             <div className="right-container">
-              <div className="stat stat-style"></div>
-              <div className="stat stat-sub-space"></div>
-              <div className="stat stat-furni-type"></div>
-              <div className="stat main-material"></div>
+              <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                  delay: 4000,
+                  disableOnInteraction: true,
+                }}
+                pagination={{
+                  clickable: true,
+                }}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+              >
+                <SwiperSlide>
+                  <div className="card-stat">
+                    <div className="stat-subtitle">Chart of Furniture Data based on</div>
+                    <div className="stat-title">Style</div>
+                    <PieChart
+                      className="pieChart1"
+                      series={[{
+                          data,
+                          highlightScope: {
+                            faded: "global",highlighted: "item",
+                          },
+                          faded: {
+                            innerRadius: 30, additionalRadius: -30, color: "gray",
+                          },},]}
+                      height={200}
+                    />
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="card-stat">Sub Space Stat</div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="card-stat">Furniture Types</div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="card-stat">Main Material</div>
+                </SwiperSlide>
+              </Swiper>
             </div>
           </div>
         </div>
