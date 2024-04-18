b78f6822d3c1fade5bd3c643b63f0d94b854d78e
unknown <nirmalapuspa1324@gmail.com>
2024-04-19 00:24:45 +0700

    left container form

--- client/src/pages/FurniDataPage.jsx
+++ client/src/pages/FurniDataPage.jsx
@@ -23,11 +23,15 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  Chip,
+  OutlinedInput,
+  Box,
   Checkbox,
   FormControlLabel,
   FormGroup,
   CircularProgress,
 } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded";
 import ListAltRoundedIcon from "@mui/icons-material/ListAltRounded";
 import Button from "@mui/material/Button";
@@ -45,6 +49,33 @@ const theme = createTheme({
     },
   },
 });
+
+// For Chip
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const materials = [
+  'Wood',
+  'Fabric',
+  'Metal',
+  'Leather',
+  'Glass',
+  'Bamboo',
+  'Plastic',
+  'Stone',
+  'Acrylic',
+  'Rubber',
+  'Wax',
+  'Foam',
+];
 
 const FurniDataPage = () => {
   const [loading, setLoading] = useState(false);
@@ -68,8 +99,11 @@ const FurniDataPage = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    // If the field being updated is 'material_tag', split the string value by commas
+    const updatedValue = name === 'material_tag' ? (typeof value === 'string' ? value.split(',') : value) : value;
+    setFormData({ ...formData, [name]: updatedValue });
     setIsFurniIdAvailable(true); // Reset availability check when the ID changes
+    console.log(formData)
   };
 
   const handleSubmit = async (e) => {
@@ -106,7 +140,7 @@ const FurniDataPage = () => {
     setLoading(true);
     try {
       console.log("Checking run")
-      const response = await axios.get(`http://localhost:3001/checkFurnitureID/${formData.furni_id}`);
+      const response = await axios.get(`/api/checkFurnitureID/${formData.furni_id}`);
       console.log(response)
       if (response.data.status === "success" && response.data.message === "already exist") {
         setIsFurniIdAvailable(false);
@@ -197,11 +231,18 @@ const FurniDataPage = () => {
       </div>
 
       <div className="modal" id="modal">
-        <div className="close-button"></div>
+        <div className="close-button" 
+            onClick={toggleForm}>
+          <CloseIcon />
+        </div>
         <form onSubmit={handleSubmit}>
-          <Typography variant="h5">Furniture Data Form</Typography>
+          <div className="title-modal">Furniture Data Form</div>
           <div className="form-container">
+
+            {/* LEFT SECTION */}
             <div className="left-container">
+
+              {/* Data Form Submission Part 1 */}
               <div className="check-container">
                 <div className="upper-part">
                   <TextField size="small" label="Furniture ID" name="furni_id" error={!isFurniIdAvailable} value={formData.furni_id} onChange={handleChange} fullWidth required />
@@ -215,32 +256,185 @@ const FurniDataPage = () => {
                   )}
                   {error && <p style={{ color: "red" }}>{error}</p>}
                   {!isFurniIdAvailable && (
-                    <p id="div-message" style={{ color: "red" }}>
-                      
-                    </p>
+                    <p id="div-message" style={{ color: "red" }}></p>
                   )}
                 </div>
               </div>
+
+              {/* Data Form Submission Part 2 */}
+              <div className="name-container">
+                <TextField sx={{ mt: 1.5 }} size="small" label="Furniture Name" name="furni_name" type="text" value={formData.furni_name} onChange={handleChange} fullWidth required />
+              </div>
+
+              {/* Data Form Submission Part 3 */}
+              <div className="space-cat-container">
+                <FormControl fullWidth size="small" sx={{ mt: 1.5 }}>
+                  <InputLabel id="demo-select-small-label">Space Category</InputLabel>
+                  <Select
+                    name="space_cat"
+                    labelId="demo-select-small-label"
+                    id="demo-select-small"
+                    value={formData.space_cat}
+                    label="Space Category"
+                    onChange={handleChange}
+                    fullWidth required
+                  >
+                    <MenuItem value={"Indoor"}>Indoor</MenuItem>
+                    <MenuItem value={"Outdoor"}>Outdoor</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+
+              {/* Data Form Submission Part 4 */}
+              <div className="sub-space-cat-container">
+                <FormControl fullWidth size="small" sx={{ mt: 1.5 }}>
+                  <InputLabel id="demo-select-small-label">Sub Space Category</InputLabel>
+                  <Select
+                    name="sub_space_cat"
+                    labelId="demo-select-small-label"
+                    id="demo-select-small"
+                    value={formData.sub_space_cat}
+                    label="Sub Space Category"
+                    onChange={handleChange}
+                    fullWidth required
+                  >
+                    <MenuItem value={"Living Room"}>Living Room</MenuItem>
+                    <MenuItem value={"Bed Room"}>Bed Room</MenuItem>
+                    <MenuItem value={"Terrace"}>Terrace</MenuItem>
+                    <MenuItem value={"Kitchen"}>Kitchen</MenuItem>
+                    <MenuItem value={"Bathroom"}>Bathroom</MenuItem>
+                    <MenuItem value={"Dining Room"}>Dining Room</MenuItem>
+                    <MenuItem value={"Dressing Room"}>Dressing Room</MenuItem>
+                    <MenuItem value={"Garden"}>Garden</MenuItem>
+                    <MenuItem value={"Balcony"}>Balcony</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+
+              {/* Data Form Submission Part 5 */}
+              <div className="furniture-type">
+                <FormControl fullWidth size="small" sx={{ mt: 1.5 }}>
+                  <InputLabel id="demo-select-small-label">Furniture Type</InputLabel>
+                  <Select
+                    name="furni_type"
+                    labelId="demo-select-small-label"
+                    id="demo-select-small"
+                    value={formData.furni_type}
+                    label="Furniture Type"
+                    onChange={handleChange}
+                    fullWidth required
+                  >
+                    <MenuItem value={"Coffee Table"}>Coffee Table</MenuItem>
+                    <MenuItem value={"Stool"}>Stool</MenuItem>
+                    <MenuItem value={"Drawer"}>Drawer</MenuItem>
+                    <MenuItem value={"Armchair"}>Armchair</MenuItem>
+                    <MenuItem value={"Chair"}>Chair</MenuItem>
+                    <MenuItem value={"Sofa"}>Sofa</MenuItem>
+                    <MenuItem value={"TV Stand"}>TV Stand</MenuItem>
+                    <MenuItem value={"Side Table"}>Side Table</MenuItem>
+                    <MenuItem value={"Cabinet"}>Cabinet</MenuItem>
+                    <MenuItem value={"Wardrobe"}>Wardrobe</MenuItem>
+                    <MenuItem value={"Dining Table"}>Dining Table</MenuItem>
+                    <MenuItem value={"Mirror"}>Mirror</MenuItem>
+                    <MenuItem value={"Dining Chair"}>Dining Chair</MenuItem>
+                    <MenuItem value={"Pouf"}>Pou</MenuItem>
+                    <MenuItem value={"Doormat"}>Doormat</MenuItem>
+                    <MenuItem value={"Desk"}>Desk</MenuItem>
+                    <MenuItem value={"Seat/Bench"}>Seat/ Bench</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+
+              {/* Data Form Submission Part 6 */}
+              <div className="furniture-style">
+                <FormControl fullWidth size="small" sx={{ mt: 1.5 }} >
+                  <InputLabel id="demo-select-small-label">Furniture Style</InputLabel>
+                  <Select
+                    name="furni_style"
+                    labelId="demo-select-small-label"
+                    id="demo-select-small"
+                    value={formData.furni_style}
+                    label="Furniture Style"
+                    onChange={handleChange}
+                    fullWidth required
+                  >
+                    <MenuItem value={"Modern"}>Modern</MenuItem>
+                    <MenuItem value={"Classy"}>Classy</MenuItem>
+                    <MenuItem value={"Minimalism"}>Minimalism</MenuItem>
+                    <MenuItem value={"Traditional"}>Traditional</MenuItem>
+                    <MenuItem value={"Industrial"}>Industrial</MenuItem>
+                    <MenuItem value={"Scandinavian"}>Scandinavian</MenuItem>
+                    <MenuItem value={"Rustic"}>Rustic</MenuItem>
+                    <MenuItem value={"Shabby Chic"}>Side Table</MenuItem>
+                    <MenuItem value={"Mid-Century Modern"}>Mid-Century Modern</MenuItem>
+                    <MenuItem value={"Japanese Zen"}>Japanese Zen</MenuItem>
+                    <MenuItem value={"Tropical/Natural"}>Tropical/Natural</MenuItem>
+                    <MenuItem value={"Classic"}>Classic</MenuItem>
+                    <MenuItem value={"Pop-Art"}>Pop-Art</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+
+              {/* Data Form Submission Part 7 */}
+              <div className="material-tag">
+                <FormControl fullWidth size="small" sx={{ mt: 1.5 }}>
+                  <InputLabel id="demo-multiple-chip-label">Material Tag</InputLabel>
+                  <Select
+                    required
+                    labelId="demo-multiple-chip-label"
+                    id="demo-multiple-chip"
+                    multiple
+                    name="material_tag"
+                    value={formData.material_tag}
+                    onChange={handleChange}
+                    input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                    renderValue={(selected) => (
+                      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                        {selected.map((value) => (
+                          <Chip key={value} label={value} />
+                        ))}
+                      </Box>
+                    )}
+                    MenuProps={MenuProps}
+                  >
+                    {materials.map((material) => (
+                      <MenuItem
+                        key={material}
+                        value={material}
+                      >
+                        {material}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </div>
+
+              {/* Data Form Submission Part 8 */}
+              <div className="detail-material">
+                <TextField
+                  sx={{ mt: 1.5, mb: 3 }}
+                  name="detail_material"
+                  id="outlined-multiline-static"
+                  label="Detail Material"
+                  onChange={handleChange}
+                  required
+                  fullWidth
+                  value={formData.detail_material}
+                  defaultValue={"Hello There"}
+                  multiline
+                  rows={3}
+                />
+              </div>
             </div>
+
+            {/* RIGHT SECTION */}
             <div className="right-container"></div>
           </div>
-          {/* <TextField
-            id="name"
-            label="Name"
-            variant="outlined"
-            value={name}
-            onChange={handleChange}
-            error={!!error}
-            helperText={error}
-            fullWidth
-            required
-          /> */}
           <div className="button-submit">
           <Button
-            onClick={toggleForm}
             type="submit"
             variant="contained"
-            color="primary"
+            color="violet"
             fullWidth
           >
             Submit
